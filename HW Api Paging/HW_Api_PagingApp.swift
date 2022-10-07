abd476f4ea0d7433695fa423f15eb0308cf930ee
Egor <egorchirkov03@gmail.com>
2022-10-07 09:33:58 +0300

    Initial Commit

---
+++ HW Api Paging/HW_Api_PagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_Api_PagingApp.swift
+//  HW Api Paging
+//
+//  Created by Егор Чирков on 07.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_Api_PagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
